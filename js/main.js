0af2cbf285ca0b36f779a99f62b05d182581965b
FrancoFlammini <francoflamminislp@gmail.com>
2023-05-02 18:17:16 -0300

    inet down

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
-const shopContent = document.getElementById("shopContent")
-const verCarrito = document.getElementById("verCarrito")
-const modalContainer = document.getElementById("modal-container")
+const shopContent = document.getElementById("shopContent");
+const verCarrito = document.getElementById("verCarrito");
+const modalContainer = document.getElementById("modal-container");
+const cantidadCarrito = document.getElementById("cantidadCarrito");
 
 
 let carrito = [];
@@ -12,7 +13,6 @@ productos.forEach((product) => {
      <img src="${product.img}">
      <h3>${product.nombre}</h3>
      <p class="price">${product.precio}$</p>
-     <p>Stock disponible ${product.stock}</p>
     `;
 
     shopContent.append(content);
@@ -24,55 +24,27 @@ productos.forEach((product) => {
     content.append(comprar);
 
     comprar.addEventListener("click", () => {
+
+    const repeat = carrito.some((repeatProduct) => repeatProduct.id === product.id);
+    
+    if(repeat){
+      carrito.map ((prod) => {
+        if(prod.id === product.id){
+          prod.cantidad++;
+        }
+      });
+    }else{
+
       carrito.push({
         id: product.id,
         img: product.img,
         nombre: product.nombre,
-        stock: product.stock,
         precio: product.precio,
+        cantidad: product.cantidad,
         
       });
-      console.log(carrito)
+    }
+      console.log(carrito);
+      carritoCounter();
     });
-});
-
-verCarrito.addEventListener("click" , () => {
-  modalContainer.innerHTML = "";
-  modalContainer.style.display = "flex";
-  const modalHeader = document.createElement("div");
-  modalHeader.className ="modal-header"
-  modalHeader.innerHTML = `
-  <h1 class="modal-header-title">Carrito.</h1>
-  `;
-  modalContainer.append(modalHeader);
-
-  const modalbutton = document.createElement("h1");
-  modalbutton.innerText = "x";
-  modalbutton.className = "modal-header-button";
-
-  modalbutton.addEventListener("click", () => {
-    modalContainer.style.display = "none";
-  });
-
-  modalHeader.append(modalbutton);
-
-
-carrito.forEach((product) => {
-  let carritoContent = document.createElement("div")
-  carritoContent.className = "modal-content";
-  carritoContent.innerHTML = `
-    <img src="${product.img}">
-    <h3> ${product.nombre}</h3>
-    <p>${product.precio}$</p>
-  `;
-
-  modalContainer.append(carritoContent);
-  });
-
-  const total = carrito.reduce((acc, el) => acc + el.precio, 0);
-
-  const totalBuying = document.createElement("div")
-  totalBuying.className = "total-content"
-  totalBuying.innerHTML = `total a pagar: ${total} $`
-  modalContainer.append(totalBuying);
 });
--- js/productos.js
+++ js/productos.js
@@ -3,30 +3,29 @@ const productos = [
         id: 1,
         nombre: "specialized", 
         precio: 5000,
-        stock: 2,
         img: "https://biciclub.com/wp-content/uploads/2022/11/90622-10_TARMAC-SL7-PRO-ETAP-CARB-SMK-METWHTSIL_FDSQ.jpg",
+        cantidad: 1,
     },
     {
         id: 2,
         nombre: "bmc",
         precio: 4500,
-        stock: 1,
         img:"https://cdn.shopify.com/s/files/1/0284/0798/8312/products/bicicleta-de-ruta-teammachine-slr-five-gray-bmc_2048x2048.jpg?v=1676064815",
+        cantidad: 1,
     },
     {
         id: 3,
         nombre: "colner",
         precio: 2550,
-        stock: 3,
         img:"https://miconibikestore.com.ar/wp-content/uploads/2023/02/Colner-Apolo-Roja.jpg",
-
+        cantidad: 1,
     },
     {
         id: 4,
         nombre: "look",
         precio: 6550,
-        stock: 1,
         img: "https://i.pinimg.com/originals/a1/01/5d/a1015de5a9e05236073487f7905f8508.jpg",
+        cantidad: 1,
     },
    
 ];
